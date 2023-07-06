a78307b81ab3ce31b9fa44aa42df8042e9d6f4ea
macko27 <macik0009@gmail.com>
2023-07-06 16:17:26 +0200

    1

--- src/Game.java
+++ src/Game.java
@@ -1,30 +1,66 @@
+import java.util.Scanner;
+
 public class Game {
+    public static final int SIZE = 9;
+    public static final int[][] winnerOptions = { {0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+                                                    {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+                                                    {0, 4, 8}, {2, 4, 6} };
     private char[] board;
-    private int size;
+    private boolean running;
 
-    public Game(int gameSize) {
-        this.board = new char[gameSize];
-        this.size = gameSize;
+    public Game() {
+        this.board = new char[SIZE];
+        this.running = true;
 
-        for (int i = 0; i < gameSize; i++) {
+        for (int i = 0; i < SIZE; i++) {
             this.board[i] = '-';
         }
     }
 
-    public void showBoard() {
-        for (int i = 0; i < Math.sqrt(this.size); i++) {
-            for (int j = 0; j < Math.sqrt(this.size); j++) {
-                System.out.print(this.board[i]);
-                System.out.print(' ');
+    public void start() {
+        showBoard();
+        while(running) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Pick: ");
+            int choice = scanner.nextInt();
+            if (choice > 0 && choice <= SIZE) {
+                if (this.board[choice - 1] == '-') {
+                    this.board[choice - 1] = 'x';
+                }
             }
-            System.out.println();
+
+
+
+            showBoard();
+            if (this.checkWinner()) running = false;
         }
     }
 
-    public boolean pickChoice(char character, int order) {
-        if (this.board[order] == '-') {
-            this.board[order] = character;
-            return true;
+    public void showBoard() {
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print(this.board[i]);
+            System.out.print(' ');
+            if (i % Math.sqrt(SIZE) == 2) {
+                System.out.println();
+            }
+        }
+    }
+
+    public boolean checkWinner() {
+        for (int i = 0; i < winnerOptions.length; i++) {
+            char option1 = this.board[winnerOptions[i][0]];
+            char option2 = this.board[winnerOptions[i][1]];
+            char option3 = this.board[winnerOptions[i][2]];
+
+            if (option1 == option2 && option1 == option3) {
+                if (option1 == 'x') {
+                    System.out.println("Player wins");
+                    return true;
+                } else if (option1 == 'o') {
+                    System.out.println("AI wins");
+                    return true;
+                }
+            }
         }
         return false;
     }
